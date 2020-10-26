0e50dddfb68de19858729633609b5faa1c08ecdf
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-10-26 16:00:30 +0500

    first commit

---
+++ src/js/game.js
@@ -0,0 +1,24 @@
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
+
+  import './domain.js'
+  
+  export class GameSavingData {
+}
+export function readGameSaving() {
+}
+export function writeGameSaving() {
+}
+export default Game;
